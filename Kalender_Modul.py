e83737203a0a0f0e14fd7e4f0a6817bd5949793e
TastingComb <61939067+TastingComb@users.noreply.github.com>
2020-03-09 17:29:15 +0100

    Add files via upload

---
+++ Kalender_Modul.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 15:19:29 2020
+
+@author: ingo
+"""
+
+import calendar
+jan=calendar.TextCalendar(calendar.MONDAY)
+jan.prmonth(2020,1)
+
+import calendar
+feb=calendar.TextCalendar(calendar.MONDAY)
+feb.prmonth(2020,2)
+
+import calendar
+mar=calendar.TextCalendar(calendar.MONDAY)
+mar.prmonth(2020,3)
+
+import calendar
+apr=calendar.TextCalendar(calendar. MONDAY)
+apr.prmonth(2020,4)
+
+import calendar
+may=calendar.TextCalendar(calendar.MONDAY)
+may.prmonth(2020,5)
+
+import calendar 
+jun=calendar.TextCalendar(calendar.MONDAY)
+jun.prmonth(2020,6)
+
+import calendar
+jul=calendar.TextCalendar(calendar.MONDAY)
+jul.prmonth(2020,7)
+
+import calendar
+aug=calendar.TextCalendar(calendar.MONDAY)
+aug.prmonth(2020,8)
+
+import calendar
+sep=calendar.TextCalendar(calendar.MONDAY)
+sep.prmonth(2020,9)
+
+import calendar
+oct=calendar.TextCalendar(calendar.MONDAY)
+oct.prmonth(2020,10)
+
+import calendar
+nov=calendar.TextCalendar(calendar.MONDAY)
+nov.prmonth(2020,11)
+
+import calendar
+dec=calendar.TextCalendar(calendar.MONDAY)
+dec.prmonth(2020,12)
